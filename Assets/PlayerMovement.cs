9a6dd9ff869c545908758ff30ee3a2898f001d4d
PalasanRares <rarespalasan@outlook.com>
2021-11-17 18:55:32 +0200

    smoothed out movement

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -6,18 +6,43 @@ public class PlayerMovement : MonoBehaviour
   public float movementSpeed;
   public float jumpForce;
 
-  private Rigidbody2D rigidbody;
+  private bool isJumping;
+  private float moveHorizontal;
+  private float moveVertical;
+
+  private Rigidbody2D _rigidbody;
 
     void Start() {
-      rigidbody = GetComponent<Rigidbody2D>();
+      _rigidbody = gameObject.GetComponent<Rigidbody2D>();
+
+      isJumping = false;
     }
 
     void Update() {
-      var movement = Input.GetAxis("Horizontal");
-      transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * movementSpeed;
+      moveHorizontal = Input.GetAxisRaw("Horizontal");
+      moveVertical = Input.GetAxisRaw("Vertical");
+    }
 
-      if (Input.GetButtonDown("Jump") && Mathf.Abs(rigidbody.velocity.y) < 0.01) {
-        rigidbody.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+    void FixedUpdate() {
+      if (moveHorizontal > 0.1f || moveHorizontal < -0.1f) {
+        _rigidbody.AddForce(new Vector2(moveHorizontal * movementSpeed, 0f), ForceMode2D.Impulse);
+      }
+
+      if (!isJumping && moveVertical > 0.1f) {
+        _rigidbody.AddForce(new Vector2(0f, moveVertical * jumpForce), ForceMode2D.Impulse);
+      }
+
+    }
+
+    void OnTriggerEnter2D(Collider2D collision) {
+      if (collision.gameObject.tag == "Platform") {
+        isJumping = false;
+      }
+    }
+
+    void OnTriggerExit2D(Collider2D collision) {
+      if (collision.gameObject.tag == "Platform") {
+        isJumping = true;
       }
     }
 }
