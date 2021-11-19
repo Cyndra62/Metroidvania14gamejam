734ae50b991bfb64657b47a6b17ad66f5f855098
PalasanRares <rarespalasan@outlook.com>
2021-11-19 22:47:30 +0200

    jumping enemy beta ;)

--- Assets/Scripts/Jumping Enemy/JumpingEnemyAI.cs
+++ Assets/Scripts/Jumping Enemy/JumpingEnemyAI.cs
@@ -9,45 +9,69 @@ namespace Jumping_Enemy
         
         [SerializeField] private Transform checkOnGround, checkHittingWall;
         [SerializeField] private LayerMask groundLayer;
-        [SerializeField] private float jumpForce, walkSpeed;
+        [SerializeField] private float jumpForce, walkSpeed, maxDistance, lineOfSight, chaseSpeed;
+        [SerializeField] private Rigidbody2D playerRigidbody;
 
         private bool _isOnGround;
         private bool _hittingWall;
+        private bool _needsToJump;
+        private bool _facingRight;
+        private bool _needsToFlip;
 
-        private SpriteRenderer _spriteRenderer;
+        private float _xOrigin;
+        private float _distanceFromOrigin;
+
+        private Vector2 _distanceToPlayer;
+        private Vector2 _currentPosition;
 
         private void Start()
         {
             _rigidbody = gameObject.GetComponent<Rigidbody2D>();
-            _spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+            _currentPosition = _rigidbody.position;
             _isOnGround = false;
             _hittingWall = false;
+            _facingRight = true;
+            _needsToFlip = false;
+            _xOrigin = _currentPosition.x;
+            _distanceFromOrigin = 0f;
+            _distanceToPlayer = _currentPosition - playerRigidbody.position;
         }
 
         private void Update()
         {
+            _currentPosition = _rigidbody.position;
+            _distanceFromOrigin = _currentPosition.x - _xOrigin;
             _isOnGround = Physics2D.OverlapCircle(checkOnGround.position, 0.1f, groundLayer);
             _hittingWall = Physics2D.OverlapCircle(checkHittingWall.position, 0.1f, groundLayer);
+            _distanceToPlayer = playerRigidbody.position - _currentPosition;
+            _needsToJump = _distanceToPlayer.x < lineOfSight || _distanceToPlayer.y < lineOfSight;
+            _needsToFlip = _distanceToPlayer.x > 0 && !_facingRight || _distanceToPlayer.x < 0 && _facingRight;
         }
 
         private void FixedUpdate()
         {
             if (_isOnGround)
             {
-                _rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+                if (_needsToJump)
+                {
+                    _rigidbody.AddForce(new Vector2(_distanceToPlayer.x * chaseSpeed, jumpForce), ForceMode2D.Impulse);
+                }
+                else
+                {
+                    _rigidbody.velocity = new Vector2(walkSpeed * Time.fixedDeltaTime, _rigidbody.velocity.y);
+                }
             }
-            if (_hittingWall)
+            if (_hittingWall || (Mathf.Abs(_distanceFromOrigin) > maxDistance && !_needsToJump) || _needsToFlip)
             {
                 Flip();
             }
-
-            _rigidbody.velocity = new Vector2(walkSpeed * Time.fixedDeltaTime, _rigidbody.velocity.y);
         }
 
         private void Flip()
         {
             walkSpeed *= -1;
             transform.localScale *= new Vector2(-1, 1);
+            _facingRight = !_facingRight;
         }
     }
 }
